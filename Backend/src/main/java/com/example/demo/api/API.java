314da209349e129427c25e53f0eb6c7b7021b968
yhuang775 <yhuang775@gatech.edu>
2023-11-27 15:20:13 -0500

    frontend api

--- Backend/src/main/java/com/example/demo/api/API.java
+++ Backend/src/main/java/com/example/demo/api/API.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = "http://192.168.1.101:8081")
 @RestController
 @RequestMapping("/api/v1")
 public class API {
